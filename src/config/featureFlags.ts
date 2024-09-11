c2e8039ba3ca685a60e21381bdaccbf4832d6988
Christian Scheub <christian.developer.app@gmail.com>
2024-09-12 00:52:29 +0200

    deactivate the logging for the web app

--- src/config/featureFlags.ts
+++ src/config/featureFlags.ts
@@ -1,7 +1,7 @@
 const featureFlag_Debug_AllLogs: boolean = false; //Show all logs in the console
-const featureFlag_Debug_Log_infoRedux: boolean = true; //Show info logs of the setter in the console
-const featureFlag_Debug_Log_Info: boolean = true; //Show info logs in the console
-const featureFlag_Debug_Log_Warning: boolean = true; //Show warning logs in the console
-const featureFlag_Debug_Log_Error: boolean = true; //Show error logs in the console
+const featureFlag_Debug_Log_infoRedux: boolean = false; //Show info logs of the setter in the console
+const featureFlag_Debug_Log_Info: boolean = false; //Show info logs in the console
+const featureFlag_Debug_Log_Warning: boolean = false; //Show warning logs in the console
+const featureFlag_Debug_Log_Error: boolean = false; //Show error logs in the console
 
 export { featureFlag_Debug_AllLogs, featureFlag_Debug_Log_infoRedux,featureFlag_Debug_Log_Error,featureFlag_Debug_Log_Warning,featureFlag_Debug_Log_Info };
